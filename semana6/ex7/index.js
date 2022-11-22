03571c73ceefdaa7dd957a6c1d510132720371d8
Kalleo Ouriques <kalleoouriques@pop-os.localdomain>
2022-11-22 19:55:56 -0300

    semana7

--- semana6/ex7/index.js
+++ semana6/ex7/index.js
@@ -1,7 +1,6 @@
 
 const url = 'https://api.coincap.io/v2/assets/bitcoin'
 
-
 async function bitCoin(){
 
     try {
@@ -9,7 +8,7 @@ async function bitCoin(){
         let response = await fetch(url)
         let data = await response.json()
         console.log(`O preço do Bitcoin - ${data.data.symbol} em dólares hoje é $ ${Math.round(parseFloat(data.data.priceUsd))}`)
-        
+
     } catch (error){
 
         console.log(error)
