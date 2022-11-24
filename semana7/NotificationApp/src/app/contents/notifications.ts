731f35d2d6ec3d51090c570422c9074e94a0f39b
Kalleo Ouriques <kalleoouriques@pop-os.localdomain>
2022-11-24 09:58:25 -0300

    semana 7 quase finalizada

---
+++ semana7/NotificationApp/src/app/contents/notifications.ts
@@ -0,0 +1,62 @@
+
+
+export const notifications = [
+
+    {   
+        title: 'Coca-Cola',
+        subtitle: 'Vende-se refrigerante sabor cola!',
+        data: 'há 10 minutos',
+        img: 'assets/images/cocaLogo.png',
+        lido: false
+    },
+    {   
+        title: 'Ype',
+        subtitle: 'Vende-se produto de limpar a casa!',
+        data: 'há 10 minutos',
+        img: 'assets/images/ypeLogo.png',
+        lido: false
+    },
+   
+    {   
+        title: 'Italac',
+        subtitle: 'Vende-se leite!',
+        data: 'há 10 minutos',
+        img: 'assets/images/italacLogo.png',
+        lido: false
+    },
+    {   
+        title: 'Tang',
+        subtitle: 'Vende-se suco em pó!',
+        data: 'há 10 minutos',
+        img: 'assets/images/tangLogo.png',
+        lido: false
+    },
+    {   
+        title: 'Nescau',
+        subtitle: 'Vende-se achocolado em pó!',
+        data: 'há 10 minutos',
+        img: 'assets/images/nescauLogo.png',
+        lido: false
+    },
+    {   
+        title: 'Soya',
+        subtitle: 'Vende-se óleo!',
+        data: 'há 10 minutos',
+        img: 'assets/images/soyaLogo.png',
+        lido: false
+    },
+    {   
+        title: 'Sadia',
+        subtitle: 'Vende-se pedaços de animais mortos!',
+        data: 'há 10 minutos',
+        img: 'assets/images/sadiaLogo.png',
+        lido: false
+    },
+    {   
+        title: 'Sazón',
+        subtitle: 'Vende-se molho sabor caldo de frango morto!',
+        data: 'há 10 minutos',
+        img: 'assets/images/sazonLogo.png',
+        lido: false
+    },
+]
